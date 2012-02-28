d2c6ded435a0d56e69791c2998e452444eee5799
Luke Opperman <luke@seabourneconsulting.com>
2012-02-28 12:45:46 -0600

    Add back in Response, SimpleSchema tests

--- test/base/Test.php
+++ test/base/Test.php
@@ -12,6 +12,8 @@
  * @link        http://cumula.org
  */
 
+require_once 'vfsStream/vfsStream.php';
+
 /**
  * BaseTest Class
  *
@@ -22,57 +24,49 @@
  * @author      Seabourne Consulting
  */
 class Test_BaseTest extends PHPUnit_Framework_TestCase {
-	/**
+    /**
      * Files to delete on tearDown
      * @var array
      */
-	protected $files = array();
-	/**
-     * setUp
-     * @param void
-     * @return void
-     **/
-	public function setUp() {
-		$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-	}
-	
+    protected $files = array();
+
 	public function assertInstance($instance, $class) {
 		self::assertTrue(isset($instance), 'Class not set');
 		self::assertTrue(get_class($instance) == $class, 'Instance not equal to class: '.get_class($instance));
 	}
 	
 	/**
-     * tearDown
+     * setUp
      * @param void
      * @return void
      **/
-	public function tearDown() {
-		if (is_array($this->files) && count($this->files) > 0)
-		{
-			foreach ($this->files as $key => $file)
-			{
-				$file = realpath($file);
-				
-				if ($file !== FALSE && file_exists($file))
-				{
-					if (is_dir($file))
-					{
-						exec('rm -rf '. escapeshellarg($file));
-					}
-					else
-					{
-						unlink($file);
-					}
-					
-					if (file_exists($file))
-					{
-						printf("Houston, We have a problem.  %s wasn\'t deleted\n", $file);
-					}
-				}
-			}
-		}
-	}
-	
-	// end function tearDown
-}
+    public function setUp() {
+			$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+			$this->setupVfs();
+    } // end function setUp
 
+		/**
+		 * Setup VFS Filestructure
+		 * @param void
+		 * @return void
+		 **/
+		private function setupVfs() 
+		{
+			vfsStream::setup('app');
+
+			$structure = array(
+				'app' => array(
+					'config' => array(),
+					'cache' => array(),
+				),
+			);
+
+			vfsStream::create($structure);
+
+			defined('APPROOT') ||
+				define('APPROOT', vfsStream::url('app'));
+			defined('CONFIGROOT') ||
+				define('CONFIGROOT', vfsStream::url('app/config'));
+		} // end function setupVfs
+
+}
